2e02c157d5d62aa3a118fb2c70180ebb74ee4aa5
Guillermo Purro <gpurro@gmail.com>
2024-04-23 11:30:54 -0300

    implementing env variables

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
     headers: {
-        Authorization: 'Bearer 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'
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`
     }
 });
 
